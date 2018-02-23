92c40e5141d592734497a8ecb068544a41f72d50
adrianancymon <adrianancymon@gmail.com>
2018-02-23 12:06:27 +0100

    Updated react setup.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>TICKER</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
